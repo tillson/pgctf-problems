d102b95f6b993fd09b9a2f94098896650ac49372
Tillson Galloway <tillson.galloway@gmail.com>
2016-09-15 09:59:49 -0400

    b0nus flag

--- web/I-cant-c/c/index.php
+++ web/I-cant-c/c/index.php
@@ -1,13 +1,17 @@
 <html>
 <head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-  <h1>Imgur 2.0 Beta</h1>
-  <p>Using middle-out compression, we now store your images with a system that has Weissman Score of 5.2</p>
-  <form action='upload.php' method='post' enctype='multipart/form-data'>
-    Select image to upload:
-    <input type='file' name='file' id='file'>
-    <input type='submit' value='Upload image' name='submit'>
-  </form>
+  <div class="container">
+    <h1>Imgur 2.0 Beta</h1>
+    <p>Using middle-out compression, we now store your images with a system that has Weissman Score of 5.2</p>
+    <form action='upload.php' method='post' enctype='multipart/form-data'>
+      Select image to upload:
+      <input type='file' name='file' id='file'>
+      <br>
+      <input class="btn btn-success" type='submit' value='Upload image' name='submit'>
+    </form>
+  </div>
 </body>
 </html>
--- web/I-cant-c/c/upload.php
+++ web/I-cant-c/c/upload.php
@@ -4,19 +4,19 @@ if (!isset($_FILES['file'])) {
   die();
 }
 
-$target_dir = "uploads/";
+$target_dir = dirname(__FILE__) . "/uploads/";
 $target_file = $target_dir . md5(time()) . pathinfo($target_file, PATHINFO_EXTENSION);
 $uploadOk = true;
 $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
-$fileName = basename($_FILES['file']['name'])
-$bannedList = array('c99.php', 'c100.php', 'r57.php', 'angel.php', 'test.php');
-foreach ($bannedList as $banned) {
-  if (strpos($fileName, $banned) !== false) {
-    echo 'You cannot upload this file.';
-    die();
-  }
-}
+// $fileName = basename($_FILES['file']['name']);
+// $bannedList = array('c99.php', 'c100.php', 'r57.php', 'angel.php', 'test.php');
+// foreach ($bannedList as $banned) {
+//   if (strpos($fileName, $banned) !== false) {
+//     echo $banned + ' is a banned file, you hacker!';
+//     die();
+//   }
+// }
 
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
     echo "Your image has been uploaded <a href='/uploads/" . basename($target_file) . "'>here</a>.";
